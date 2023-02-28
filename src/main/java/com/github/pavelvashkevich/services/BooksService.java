36d61b4f3523ff0539de1460a491425f04324d37
Pavel Vashkevich <veider300@gmail.com>
2023-02-28 23:06:37 +0100

    Added Services for Books and People

--- src/main/java/com/github/pavelvashkevich/services/BooksService.java
+++ src/main/java/com/github/pavelvashkevich/services/BooksService.java
@@ -1,2 +1,65 @@
-package com.github.pavelvashkevich.services;public class BooksService {
+package com.github.pavelvashkevich.services;
+
+import com.github.pavelvashkevich.model.Book;
+import com.github.pavelvashkevich.model.Person;
+import com.github.pavelvashkevich.repositories.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class BooksService {
+
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BooksService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Book> findAll() {
+        return booksRepository.findAll();
+    }
+
+    public Book findOne(int id) {
+        Optional<Book> book = booksRepository.findById(id);
+        return book.orElse(null);
+    }
+
+    @Transactional
+    public void save(Book book) {
+        booksRepository.save(book);
+    }
+
+    public Person findPatronInfo(int id) {
+        Optional<Book> book = booksRepository.findById(id);
+        return book.map(Book::getPatron).orElse(null);
+    }
+
+    @Transactional
+    public void update(int id, Book updatedBook) {
+        updatedBook.setId(id);
+        booksRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        booksRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void assignPatron(int id, Person patron) {
+        Optional<Book> bookToAssignPatron = booksRepository.findById(id);
+        bookToAssignPatron.ifPresent(book -> book.setPatron(patron));
+    }
+
+    @Transactional
+    public void releaseBook(int id) {
+        Optional<Book> bookToRelease = booksRepository.findById(id);
+        bookToRelease.ifPresent(book -> book.setPatron(null));
+    }
 }
--- src/main/java/com/github/pavelvashkevich/services/PeopleService.java
+++ src/main/java/com/github/pavelvashkevich/services/PeopleService.java
@@ -1,2 +1,59 @@
-package com.github.pavelvashkevich.services;public class PeopleService {
+package com.github.pavelvashkevich.services;
+
+import com.github.pavelvashkevich.model.Book;
+import com.github.pavelvashkevich.model.Person;
+import com.github.pavelvashkevich.repositories.PeopleRepository;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> person = peopleRepository.findById(id);
+        return person.orElse(null);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+
+    public List<Book> findPersonBooks(int id) {
+        Optional<Person> person = peopleRepository.findById(id);
+        if (person.isPresent()) {
+            Hibernate.initialize(person.get().getBooks());
+            return person.get().getBooks();
+        }
+        return Collections.emptyList();
+    }
 }
